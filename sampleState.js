3a8633ad98e8fbf09722a7dfce52fb13a1abb95d
cindytong <tongcindyy@gmail.com>
2020-01-30 15:30:15 -0500

    set up sample state

---
+++ sampleState.js
@@ -0,0 +1,16 @@
+{ "todos": {
+    "byId": {
+        "1": {
+            id: "1",
+            text: "Finish homework",
+            done: false  
+        },
+        "2": {
+            id: "2",
+            text: "Pick up milk",
+            done: false  
+        }
+    },
+    "allIds": ["1", "2"] 
+    }
+}
